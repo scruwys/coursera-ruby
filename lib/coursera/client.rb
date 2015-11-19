6303aee5f4724d02c200ca9dced0252e07e1c500
Scott Cruwys <scruwys@gmail.com>
2015-11-19 03:38:03 +0000

    Added search functionality.

--- lib/coursera/client.rb
+++ lib/coursera/client.rb
@@ -8,5 +8,15 @@ module Coursera
     def initialize(options = {})
       @options = options
     end
+
+    def find_course(id, attrs = {})
+      Coursera::Course.find id, attrs
+    end
+
+    # => client.search_courses("data analytics", { fields: [:name, :photo] })
+    def search_courses(query, attrs = {})
+      attrs = self.class.prepare_attributes(attrs)
+      Coursera::Course.batch_request("/courses.v1?q=search", { query: query}.merge(attrs))
+    end
   end
 end
